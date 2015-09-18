4c363a3572a81102efd3a4d72f599b33a2420558
Mo Moulton <mo.moulton22@gmail.com>
2015-09-18 09:35:34 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+error_reporting(-1); # Report all PHP errors
+ini_set('display_errors', 1);
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>xkcd Passwords</title>
+  <?php require 'logic.php'; ?>
+</head>
+
+<body>
+  hullouloo
+</body>
+
+</html>
