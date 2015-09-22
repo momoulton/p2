f48437c5b30ffcd244d614880963784b5250dad6
Mo Moulton <mo.moulton22@gmail.com>
2015-09-22 12:25:30 -0400

    working on logic

--- index.php
+++ index.php
@@ -13,8 +13,14 @@ ini_set('display_errors', 1);
 
 <body>
   <h1>A Very Literary xkcd Password Generator</h1>
-  <p>The web comic xkcd proposed a method of generating passwords that are easy for people and hard for machines. This app generates xkcd passwords for those with bookish sensibilities.</p>
-  <form>
+  <p>The web comic xkcd proposed a method of generating passwords which are easy for people to remembers and hard for machines to guess. This app generates xkcd passwords for those with bookish sensibilities.</p>
+  <h3><?php  for ($i = 0; $i < count($display); $i++) {
+    echo $display[$i] . " ";
+  }
+  ?></h3>
+  <p><?php echo $error; ?></p>
+
+  <form method="GET" action="index.php" >
     How many words? (1-9) <input type="text" name="words" maxlength="1">
     <br>
     How many numbers? (0-9) <input type="text" name="nums" maxlength="1">
@@ -23,14 +29,15 @@ ini_set('display_errors', 1);
     <br>
     Which book?
     <br>
-    <input type="radio" name="book" value="Pride and Prejudice (Austen)" checked>Pride and Prejudice (Austen)
+    <input type="radio" name="book" value="Pride and Prejudice" checked>Pride and Prejudice (Austen)
     <br>
-    <input type="radio" name="book" value="Jane Eyre (Brontë)">Jane Eyre (Brontë)
+    <input type="radio" name="book" value="Jane Eyre">Jane Eyre (Brontë)
     <br>
-    <input type="radio" name="book" value="Middlemarch (Eliot)">Middlemarch (Eliot)
+    <input type="radio" name="book" value="Middlemarch">Middlemarch (Eliot)
     <br>
-    <input type="radio" name="book" value="Greak Expectations (Dickens)">Greak Expectations (Dickens)
+    <input type="radio" name="book" value="Great Expectations">Great Expectations (Dickens)
     <br>
+    <input type='submit' value='Generate a password'>
   </form>
   <img src='images/password_strength.png' alt='xkcd cartoon on passwords'/>
 </body>
---
+++ logic.php
@@ -0,0 +1,41 @@
+<?php
+  $numwords = $_GET['words'];
+  $numdigits = $_GET['nums'];
+  $numspecs = $_GET['specials'];
+  $book = $_GET['book'];
+  $display = array("correct", "horse", "battery", "staple");
+  $error = "";
+
+  $digits = array('1', '2', '3', '4', '5', '6', '7', '8', '9');
+  $specials = array('!', '@', '#', '$', '%', '?', '&', '*');
+
+  if (!empty($numwords) AND (!is_numeric($numwords) OR $numwords < 1 OR $numwords > 9)) {
+    $error = "Error: please enter a valid integer 1-9 for the number of words";
+  }
+  elseif (!empty($numdigits) AND (!is_numeric($numdigits) OR $numdigits < 0 OR $numdigits > 9)) {
+    $error= "Error: please enter a valid integer 0-9 for the number of numbers";
+  }
+  elseif (!empty($numspecs) AND (!is_numeric($numspecs) OR $numspecs < 0 OR $numspecs > 9)) {
+    $error = "Error: please enter a valid integer 0-9 for the number of special characters";
+  }
+  else {
+    $display = array();
+    if (!empty($numdigits)) {
+      for ($i=0; $i < $numdigits; $i++) {
+        $numchoice = rand(0,count($digits)-1);
+        array_push($display, $digits[$numchoice]);
+      }
+    }
+    if (!empty($numspecs)) {
+      for ($i=0; $i < $numspecs; $i++) {
+        $specchoice = rand(0,count($specials)-1);
+        array_push($display, $specials[$specchoice]);
+      }
+    }
+    shuffle($display);
+  }
+
+
+
+
+ ?>
